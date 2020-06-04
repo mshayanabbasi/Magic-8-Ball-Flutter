63c605ce11f0afb347f2c08e2d7511cd1f3297b4
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-06-04 15:32:59 +0500

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue[900],
+          title: Text(
+            'Ask Me Anything'
+          ),
+        ),
+        body: MagicBall(),
+      ),
+    );
+  }
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
       
     );
   }
